c137e02b47d380430a2d9a478cc386440da18ccb
Andressa Gaspar <andressa.luiz@softdesign.com.br>
2020-02-19 17:11:11 -0300

    componentizando os cards

--- components/CardButtonHome.js
+++ components/CardButtonHome.js
@@ -1,33 +1,41 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 
-function CardButtonHome({ card, icon }) {
+function CardButtonHome({ card, icon, text, title }) {
     return (
         <TouchableOpacity>
-            <SvgXml xml={card} style={styles.card} />
-            <Text style={styles.title} />
-            <SvgXml xml={icon} style={styles.icon} />
-            <Text style={styles.text} />
+            {/* <View backgroundColor={red}> */}
+            <SvgXml width={"100%"} top={20} xml={card} />
+            <Text style={styles.title}> {`${title}`} </Text>
+            <SvgXml width={"600"} position={"absolute"} bottom={70} xml={icon} />
+            <Text style={styles.text}> {`${text}`} </Text>
+            {/* </View> */}
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
-    card: {
-        width: 288,
-        alignItens: "center",
-    },
     title: {
         color: '#fff',
-    },
-    icon: {
-
+        left: 95,
+        bottom: 95,
+        position: "absolute",
+        maxWidth: 200
     },
     text: {
         color: '#fff',
+        left: 95,
+        bottom: 20,
+        position: "relative",
+        maxWidth: 200
     },
 });
+
+// const card = styled.View`
+
+
+// `
 
 export default CardButtonHome;
 
--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import info from '../assets/imgs/info';
+import { View } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 import { Header } from 'react-native-elements';
 import CardButtonHome from '../components/CardButtonHome';
-import CardGame from '../assets/imgs/CardGame';
-import InconGame from '../assets/imgs/IconGame';
+import blue from '../assets/imgs/CardGame/blue';
+import rectangle from '../assets/imgs/CardGame/rectangle'
+import iconTI from '../assets/imgs/IconGame/iconTI'
 
 const Home = () => {
 
   renderCardGame = () => {
-    <CardButtonHome />
+    return (
+      <View>
+        <CardButtonHome
+          card={blue}
+          icon={iconTI}
+          title={"gggggggggggg"}
+          text={'tchau vfdrgrg grgrg'}
+        />
+        <CardButtonHome
+          card={rectangle}
+          icon={iconTI}
+        />
 
+        <CardButtonHome
+          card={rectangle}
+          icon={iconTI}
+        />
+      </View>
+    )
   }
 
 
   return (
-    <Header
-      backgroundColor={'#F07E26'}
-      rightComponent={<SvgXml xml={info} />}
-      centerComponent={{
-        style: { color: '#fff', fontSize: 20 },
-        text: 'GameSoft',
-      }}
-    />
+    <>
+      <Header
+        backgroundColor={'#F07E26'}
+        rightComponent={<SvgXml xml={info} />}
+        centerComponent={{
+          style: { color: '#fff', fontSize: 20 },
+          text: 'GameSoft',
+        }}
+      />
+      {renderCardGame()}
+    </>
   );
 };
 
